79eeac0cccece40d075ca113b671b13b807d948d
Murken <muarvievvlad.2002@gmail.com>
2023-09-15 00:41:56 +0300

    refactor xml

--- Helloapp/Files/XmlFile.cs
+++ Helloapp/Files/XmlFile.cs
@@ -2,23 +2,22 @@
 using System.Xml.Serialization;
 using Helloapp.Entities;
 
-namespace Helloapp.Files
+namespace Helloapp.Files;
+
+public class XmlFile : FileBase
 {
-	public class XmlFile : FileBase
+	public XmlFile() : base(".xml") { }
+
+	public override string Insert(object data)
 	{
-		public XmlFile() : base(".xml") { }
+		if (data is Student == false) throw new ArgumentException("Wrong type");
 
-		public override string Insert(object data)
-		{
-			if (data is Student == false) throw new ArgumentException("Wrong type");
+		if (_info == null || !_info.Exists) return "Файл необходимо создать перед использованием!";
 
-			if (_info == null || !_info.Exists) return "Файл необходимо создать перед использованием!";
+		using var fs = _info.Create();
+		var serializer = new XmlSerializer(typeof(Student));
+		serializer.Serialize(fs, (Student)data);
 
-			using var fs = _info.Create();
-			var serializer = new XmlSerializer(typeof(Student));
-			serializer.Serialize(fs, (Student)data);
-
-			return "Файл успешно создан";
-		}
+		return "Файл успешно создан";
 	}
 }
