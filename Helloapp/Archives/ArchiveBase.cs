22e83cd5192c3b71597ee4605ac614f9220c3ccd
Murken <muarvievvlad.2002@gmail.com>
2023-09-15 00:53:36 +0300

    refactor archive base

--- Helloapp/Archives/ArchiveBase.cs
+++ Helloapp/Archives/ArchiveBase.cs
@@ -1,37 +1,36 @@
 ﻿using System;
 using System.IO;
 
-namespace Helloapp.Archives
+namespace Helloapp.Archives;
+
+public abstract class ArchiveBase
 {
-	public abstract class ArchiveBase
+	protected string _dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+	protected string _format;
+	protected FileInfo _fileInfo;
+	protected FileInfo _info;
+
+	protected ArchiveBase(string format)
 	{
-		protected string _dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-		protected string _format;
-		protected FileInfo _fileInfo;
-		protected FileInfo _info;
-
-		protected ArchiveBase(string format)
-		{
-			_format = format;
-		}
-
-		public abstract string Create(string archiveName);
-
-		public abstract string AddFile(string fileName);
-
-		public abstract string Unzip();
-
-		public string Delete()
-		{
-			if (_info == null || !_info.Exists)
-				return "Архив необходимо создать перед удалением!";
-			if (_fileInfo == null || !_fileInfo.Exists)
-				return "Файл необходимо разархифировать перед удалением!";
-
-			_info.Delete();
-			_fileInfo.Delete();
-			return "Архив и файл удалены успешно";
-		}
-
+		_format = format;
 	}
+
+	public abstract string Create(string archiveName);
+
+	public abstract string AddFile(string fileName);
+
+	public abstract string Unzip();
+
+	public string Delete()
+	{
+		if (_info == null || !_info.Exists)
+			return "Архив необходимо создать перед удалением!";
+		if (_fileInfo == null || !_fileInfo.Exists)
+			return "Файл необходимо разархифировать перед удалением!";
+
+		_info.Delete();
+		_fileInfo.Delete();
+		return "Архив и файл удалены успешно";
+	}
+
 }
